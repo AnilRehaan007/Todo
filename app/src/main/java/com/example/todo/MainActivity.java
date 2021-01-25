7e45b9b6ac9b6d489b5210d0c07a6780b83267d9
anil rehaan <anilr0984211@gmail.com>
2021-01-25 14:10:33 +0530

    Todo

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements Profile.CallbackP
     public static String callBackFromWhere="empty";
     public static boolean editorCaller=false;
     public static boolean startService=false;
-    public static final String demo="demo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
